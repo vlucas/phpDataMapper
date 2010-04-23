4eca7951269499c9bd607417b0191b89d4c67dc5
Vance Lucas <vance@vancelucas.com>
2010-04-23 17:16:37 -0500

    Tests > CRUD: Updated insert test to use correct adapter date function for setting date

--- tests/Test/CRUD.php
+++ tests/Test/CRUD.php
@@ -36,10 +36,10 @@ class Test_CRUD extends PHPUnit_Framework_TestCase
 		$post = $mapper->get();
 		$post->title = "Test Post";
 		$post->body = "<p>This is a really awesome super-duper post.</p><p>It's really quite lovely.</p>";
-		$post->date_created = date($mapper->adapter()->dateTimeFormat());
+		$post->date_created = $mapper->adapter()->date();
 		$result = $mapper->insert($post); // returns an id
 		
-		$this->assertTrue(is_numeric($result));
+		$this->assertTrue($result !== false);
 	}
 	
 	public function testSampleNewsInsertWithEmpyNonRequiredFields()
